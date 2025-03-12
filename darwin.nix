66284f7ef30084459b741972a49b3809564cb81a
Chris Moore <chris@cdmwebs.com>
2025-03-12 15:42:57 -0400

    darwin updates

--- darwin.nix
+++ darwin.nix
@@ -105,10 +105,7 @@
     };
   };
 
-  security.pam.enableSudoTouchIdAuth = true;
-
-  # Auto upgrade nix package and the daemon service.
-  services.nix-daemon.enable = true;
+  security.pam.services.sudo_local.touchIdAuth = true;
 
   nix = {
     package = pkgs.nix;
